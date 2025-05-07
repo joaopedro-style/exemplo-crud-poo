10c421e443c993162e175f80510d4ab96704c4b2
joaopedro-style <joaopedro18231@hotmail.com>
2025-05-07 09:35:18 -0300

    Classe FabricantesServicos metodo listar todos

---
+++ src/Services/FabricanteServico.php
@@ -0,0 +1,29 @@
+<?php
+namespace ExemploCrud\Services;
+
+use Exception;
+use PDO;
+use Throwable;
+
+use ExemploCrud\ConexaoBD;
+
+
+final class FabricanteServico {
+    private PDO $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = ConexaoBD::getConexao();
+    }
+
+    public function listarTodos():array {
+        $sql = "SELECT * FROM fabricantes ORDER BY nome";
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->execute();
+            return $consulta->fetchALL(PDO::FETCH_ASSOC);
+        } catch (Throwable $erro) {
+            throw new Exception("Erro ao carregar fabricantes: ".$erro->getMessage());
+        }
+    }
+}
