9419062ebdb2fbf0297c0dff7e9319f9bb3fbc0b
joaopedro-style <joaopedro18231@hotmail.com>
2025-05-07 09:02:33 -0300

    Adiciona get e set

--- src/Models/Fabricante.php
+++ src/Models/Fabricante.php
@@ -1,6 +1,8 @@
 <?php
 namespace ExemploCrud;
 
+use InvalidArgumentException;
+
 final class Fabricante {
     private ?int $id;
     private string $nome;
@@ -10,5 +12,32 @@ final class Fabricante {
         $this->setNome($nome);
         $this->setId($id);
         $this->validar();
+    }
+
+    private function validar():void 
+    {
+        if (empty($this->nome)){
+            throw new InvalidArgumentException("Nome é obrigatório");
+        }
+    }
+
+    public function getId(): ?int 
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string 
+    {
+        return $this->nome;
+    }
+
+    private function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    private function setNome(string $nome): void
+    {
+        $this->nome = $nome;
     }
 }
