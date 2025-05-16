532943743e662bc056c22cd3be4d2e8c848b933f
joaopedro-style <joaopedro18231@hotmail.com>
2025-05-16 10:36:16 -0300

    Método arquivo registra de log

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -20,4 +20,17 @@ final class Utils
     {
         return "R$" . number_format($valor, 2, ",", ".");
     }
+
+    public static function registrarLog(Throwable $e): void
+    {
+        date_default_timezone_set('America/Sao_Paulo');
+
+        $mensagem = "[" . date("Y-m-d H:i:s") . "]\n" .
+            "Arquivo: " . $e->getFile() . "\n" .
+            "Linha: " . $e->getLine() . "\n" .
+            "Mensagem: " . $e->getMessage() . "\n\n";
+
+
+        file_put_contents(__DIR__ . '/../../logs/erros.log', $mensagem, FILE_APPEND);
+    }
 }
